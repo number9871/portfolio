96a4e4f3c600c570ef51ac759e1204954bd365c5
Artem Musin NB <war-data@yandex.ru>
2022-07-04 14:50:58 +0300

    Finished the desktop version layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,23 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close'),
-      overlay = document.querySelector('.menu__overlay');
+	  menu = document.querySelector('.menu'),
+	  closeElem = document.querySelector('.menu__close'),
+	  overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+	menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+	menu.classList.remove('active');
 });
 
 overlay.addEventListener('click', () => {
-    menu.classList.remove('active');
+	menu.classList.remove('active');
+});
+
+const percents = document.querySelectorAll('.skills__percent-progress'),
+	  lines = document.querySelectorAll('.skills__percent-positive');
+
+percents.forEach( (item, i) => {
+	lines[i].style.width = item.innerHTML;
 });
